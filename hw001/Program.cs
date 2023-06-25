50626d99651815e72ce07e7ef1968e2c9a2a29c8
Irina <irina@Mac-mini-Irina.local>
2023-06-25 17:29:38 +0500

    ДЗ1

--- hw001/Program.cs
+++ hw001/Program.cs
@@ -1,25 +1,18 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Clear();
-System.Console.WriteLine("Выведм все натуральные числа в промежутке от N до 1");
-System.Console.WriteLine("Введите целое число N");
-int num = Convert.ToInt32 (Console.ReadLine());
-
+// Console.Clear();
+Console.WriteLine("Ввыведем все натуральные числа в промежутке от N до 1");
+Console.Write("Введите целое число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
 Console.Write("Натуральные числа в промежутке от "+num+" до 1: ");
-
-string Rec (int num)
+int count = 1;
+Array (num, count);
+void Array(int n, int count)
 {
-    if (num == 0) return 0;
-    
-for (int count = 1; count <= num; count++)
-        Console.Write(count+", ");
-
+    if (count > n) return;
+    else Array (n, count + 1);
+    Console.Write(count + " ");
 }
-// string SummaRec (int num, int num2)
-// {
-//     if (num <= num2) return num+", ";
-// else return SummaRec (num - 1, num2) + $"{num}, ";
-// }
-// System.Console.WriteLine(SummaRec (num, num2));
