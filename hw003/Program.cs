5a960830721cc30cead7f2f38d8ab375694be6d3
Irina <irina@Mac-mini-Irina.local>
2023-06-25 17:15:53 +0500

    ДЗ3

--- hw003/Program.cs
+++ hw003/Program.cs
@@ -1,17 +1,21 @@
-﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-Console.Write(«Введите значение M: «);
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write(«Введите значение N: «);
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+Console.WriteLine("Вычислим значение функции Аккермана");
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
 
-int akkerman(int m, int n)
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+int Akkerman(int M, int N)
 {
-if (m == 0) return n + 1;
-else if (n == 0) return akkerman(m — 1, 1);
-else return akkerman(m — 1, akkerman(m, n — 1));
+    if (M == 0) return N + 1;
+    else if (N == 0 && M > 0) return Akkerman(M - 1, 1);
+    else return (Akkerman(M - 1, Akkerman(M, N - 1)));
 }
-
-Console.Write($»Функция Аккермана равно {akkerman(m, n)} «);
+Console.WriteLine($"Функция Аккермана равнa {Akkerman(M, N)}");
