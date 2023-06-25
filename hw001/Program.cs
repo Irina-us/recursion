238806dd58ef3df4ae760e610f16b5ce70905ec2
Irina <irina@Mac-mini-Irina.local>
2023-06-25 16:58:55 +0500

    ДЗ1

--- hw001/Program.cs
+++ hw001/Program.cs
@@ -4,11 +4,22 @@
 
 Console.Clear();
 System.Console.WriteLine("Выведм все натуральные числа в промежутке от N до 1");
-System.Console.WriteLine("Введите целое число");
-int num = Convert.ToInt32( Console.ReadLine() );
+System.Console.WriteLine("Введите целое число N");
+int num = Convert.ToInt32 (Console.ReadLine());
+
 Console.Write("Натуральные числа в промежутке от "+num+" до 1: ");
 
+string Rec (int num)
+{
+    if (num == 0) return 0;
+    
 for (int count = 1; count <= num; count++)
         Console.Write(count+", ");
 
-
+}
+// string SummaRec (int num, int num2)
+// {
+//     if (num <= num2) return num+", ";
+// else return SummaRec (num - 1, num2) + $"{num}, ";
+// }
+// System.Console.WriteLine(SummaRec (num, num2));
