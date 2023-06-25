b593d7f2cf38ff3940f09713b64df4a36d4a8016
Irina <irina@Mac-mini-Irina.local>
2023-06-25 15:30:53 +0500

    ДЗ1

--- hw001/Program.cs
+++ hw001/Program.cs
@@ -6,45 +6,9 @@ Console.Clear();
 System.Console.WriteLine("Выведм все натуральные числа в промежутке от N до 1");
 System.Console.WriteLine("Введите целое число");
 int num = Convert.ToInt32( Console.ReadLine() );
-Console.Write("Натуральные числа в промежутке от "+num+"до 1 : ");
+Console.Write("Натуральные числа в промежутке от "+num+" до 1: ");
+
 for (int count = 1; count <= num; count++)
-    if (count % 1==0)
         Console.Write(count+", ");
 
 
-
-// Console.WriteLine("Введите через запятую целые числа и нажмите Enter");
-// string[] nums = Console.ReadLine().Split(new char[] { ',' });
-// int[] intArray = new int[nums.Length];
-// for (int i = 0; i < nums.Length; i++)
-// {
-// intArray[i] = int.Parse(nums[i]);
-// }
-// Console.Write($"Неотсортированный массив:");
-// foreach (int value in intArray)
-// {
-// Console.Write($"{value} ");
-// }
-// int indx; //переменная для хранения индекса минимального элемента массива
-// for (int i = 0; i < intArray.Length; i++) //проходим по массиву с начала и до конца
-// {
-// indx = i; //считаем, что минимальный элемент имеет текущий индекс 
-// for (int j = i; j < intArray.Length; j++) //ищем минимальный элемент в неотсортированной части
-// {
-// if (intArray[j] < intArray[indx])
-// {
-// indx = j; //нашли в массиве число меньше, чем intArray[indx] - запоминаем его индекс в массиве
-// }
-// }
-// if (intArray[indx] == intArray[i]) //если минимальный элемент равен текущему значению - ничего не меняем
-// continue;
-// //меняем местами минимальный элемент и первый в неотсортированной части
-// int temp = intArray[i]; //временная переменная, чтобы не потерять значение intArray[i]
-// intArray[i] = intArray[indx];
-// intArray[indx] = temp;
-// }
-// Console.WriteLine("\r\nОтсортированный массив:");
-// foreach (int value in intArray)
-// {
-// Console.Write($"{value} ");
-// }
